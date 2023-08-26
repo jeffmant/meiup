0cb3618a4aea330dc4543b851ff70b1e6654df96
Jefferson Mantovani <jefferson.mantovani@bolttech.io>
2023-08-26 11:44:10 -0300

    feat: create page before dashboard to add company to logged user

--- src/app/api/user/route.js
+++ src/app/api/user/route.js
@@ -2,27 +2,58 @@ import { currentUser } from '@clerk/nextjs'
 import { PrismaClient } from '@prisma/client'
 import { NextResponse } from 'next/server'
 
-export async function POST (req) {
-  const { id: clerkUserId } = await currentUser()
-  const userBody = await req.json()
+export async function GET (_req) {
+  try {
+    const { id: clerkUserId } = await currentUser()
 
-  const prisma = new PrismaClient()
+    const prisma = new PrismaClient()
 
-  const foundUser = await prisma.user.findFirst({ where: { clerkUserId } })
+    const foundUser = await prisma.user.findFirstOrThrow({ where: { clerkUserId } })
 
-  if (foundUser) {
-    throw new Error('User already exists')
+    return NextResponse.json({
+      data: foundUser
+    }, {
+      status: 200
+    })
+  } catch (error) {
+    return NextResponse.json({
+      error: error.message || 'Internal Server Error'
+    }, {
+      status: error.statusCode || 500
+    })
   }
+}
 
-  await prisma.user.create({
-    data: {
-      ...userBody,
-      clerkUserId
+export async function POST (req) {
+  try {
+    const userBody = await req.json()
+
+    const prisma = new PrismaClient()
+
+    const foundUser = await prisma.user.findFirst({
+      where: {
+        foundUser: userBody.foundUser
+      }
+    })
+
+    if (foundUser) {
+      throw new Error('User already exists')
     }
-  })
 
-  return NextResponse.json({
-    success: true,
-    status: 201
-  })
+    const createdUser = await prisma.user.create({
+      data: userBody
+    })
+
+    return NextResponse.json({
+      data: createdUser
+    }, {
+      status: 201
+    })
+  } catch (error) {
+    return NextResponse.json({
+      error: error.message || 'Internal Server Error'
+    }, {
+      status: error.statusCode || 500
+    })
+  }
 }
