8443b512635e6dbf177e2683ff66a6b5b3dd78b2
Jefferson Mantovani <jefferson.mantovani@bolttech.io>
2023-09-08 21:37:02 -0300

    refactor: send user if it havent company

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -14,6 +14,7 @@ import {
 } from '@mui/material'
 import { Stack } from '@mui/system'
 import Head from 'next/head'
+import { useRouter } from 'next/navigation'
 import { useEffect, useState } from 'react'
 import { TransactionCardList } from 'src/components/Transaction/TransactionCardList'
 import TransactionMonthSelector from 'src/components/Transaction/TransactionMonthSelector'
@@ -28,6 +29,7 @@ const currentYear = new Date().getFullYear()
 
 const Dashboard = () => {
   const lgUp = useMediaQuery((theme) => theme.breakpoints.up('lg'))
+  const [userHasCompany, setUserHasCompany] = useState(false)
   const [openTransactionModal, setOpenTransactionModal] = useState(false)
   const [transactionType, setTransactionType] = useState('all')
   const [selectedTransaction, setSelectedTransaction] = useState(null)
@@ -40,9 +42,39 @@ const Dashboard = () => {
   const [page, setPage] = useState(1)
   const limit = 10
 
+  const { push } = useRouter()
   const { getToken } = useAuth()
 
   const mdUp = useMediaQuery((theme) => theme.breakpoints.up('md'))
+
+  async function getUser () {
+    const userToken = await getToken()
+    const { data: foundUser } = await fetch('/api/user', {
+      method: 'GET',
+      headers: { Authorization: `Bearer ${userToken}` }
+    }).then(response => response.json())
+
+    return foundUser
+  }
+
+  async function getUserCompanies () {
+    const userToken = await getToken()
+    const user = await getUser()
+    if (user) {
+      const response = await fetch(`/api/company/?userId=${user?.id}`, {
+        headers: { Authorization: `Bearer ${userToken}` }
+      }).then(response => response.json())
+
+      const userCompanies = response?.data || []
+
+      if (userCompanies?.length > 0) setUserHasCompany(true)
+      else push('/')
+    }
+  }
+
+  useEffect(() => {
+    getUserCompanies()
+  }, [])
 
   const refreshTransactions = async () => {
     const accessToken = await getToken()
@@ -75,212 +107,214 @@ const Dashboard = () => {
     refreshTransactions()
   }, [transactionYear, transactionMonth, transactionType, page])
 
-  return (
-    <>
-      <Head>
-        <title>meiup</title>
-      </Head>
-      <Box
-        component='main'
-        sx={{
-          flexGrow: 1,
-          py: 2
-        }}
-      >
-        <Container maxWidth='xl'>
-          <Stack
-            alignItems='center'
-            direction={mdUp ? 'row' : 'column'}
-            justifyContent={mdUp ? 'space-between' : 'center'}
-            spacing={4}
-            sx={{ mb: 2 }}
-          >
-            <Card
-              sx={{
-                display: 'flex',
-                direction: 'column',
-                justifyContent: 'space-around',
-                height: '100%',
-                width: 512,
-                p: 2
-              }}
+  if (userHasCompany) {
+    return (
+      <>
+        <Head>
+          <title>meiup</title>
+        </Head>
+        <Box
+          component='main'
+          sx={{
+            flexGrow: 1,
+            py: 2
+          }}
+        >
+          <Container maxWidth='xl'>
+            <Stack
+              alignItems='center'
+              direction={mdUp ? 'row' : 'column'}
+              justifyContent={mdUp ? 'space-between' : 'center'}
+              spacing={4}
+              sx={{ mb: 2 }}
             >
-              <Stack
-                alignItems='center'
-                direction='row'
-                justifyContent='space-between'
-                spacing={4}
-              >
-                <div>
-                  <Typography
-                    color='text.secondary'
-                    variant='overline'
-                    align='center'
-                  >
-                    Receitas
-                  </Typography>
-                </div>
-                <div>
-                  <Typography variant='h4'>
-                    {monthlyRevenue.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}
-                  </Typography>
-                </div>
-              </Stack>
-            </Card>
-            <Card
-              sx={{
-                display: 'flex',
-                direction: 'column',
-                justifyContent: 'space-around',
-                height: '100%',
-                width: 512,
-                p: 2
-              }}
-            >
-              <Stack
-                alignItems='center'
-                direction='row'
-                justifyContent='space-between'
-                spacing={4}
-              >
-                <div>
-                  <Typography
-                    color='text.secondary'
-                    gutterBottom
-                    variant='overline'
-                    align='center'
-                  >
-                    Despesas
-                  </Typography>
-                </div>
-                <div>
-                  <Typography variant='h4'>
-                    {monthlyCost.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}
-                  </Typography>
-                </div>
-              </Stack>
-            </Card>
-          </Stack>
-          <Grid
-            container
-            spacing={3}
-          >
-            <Grid xs={12}>
               <Card
                 sx={{
-                  minHeight: '70vh',
+                  display: 'flex',
+                  direction: 'column',
+                  justifyContent: 'space-around',
                   height: '100%',
-                  width: '100%'
+                  width: 512,
+                  p: 2
                 }}
               >
-                <CardContent>
-                  <Box
-                    sx={{
-                      display: 'flex',
-                      flexDirection: mdUp ? 'row' : 'column',
-                      justifyContent: 'space-between',
-                      maxHeight: '65px',
-                      alignItems: 'center',
-                      gap: '10px'
-                    }}
-                  >
-                    <Stack>
-                      <Stack sx={{ flexDirection: 'row' }}>
-                        <Stack sx={{ mr: 2 }}>
-                          <TransactionMonthSelector
-                            handleTransactionMonth={handleTransactionMonth}
-                          />
-                        </Stack>
-
-                        <Stack sx={{ mr: 2 }}>
-                          <TransactionYearSelector handleTransactionYear={handleTransactionYear} />
-                        </Stack>
-
-                        <Stack sx={{ mr: 2 }}>
-                          <TransactionTypeSelector handleTransactionType={handleTransactionType} />
-                        </Stack>
-
-                      </Stack>
-                    </Stack>
-                    <Stack
+                <Stack
+                  alignItems='center'
+                  direction='row'
+                  justifyContent='space-between'
+                  spacing={4}
+                >
+                  <div>
+                    <Typography
+                      color='text.secondary'
+                      variant='overline'
+                      align='center'
+                    >
+                      Receitas
+                    </Typography>
+                  </div>
+                  <div>
+                    <Typography variant='h4'>
+                      {monthlyRevenue.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}
+                    </Typography>
+                  </div>
+                </Stack>
+              </Card>
+              <Card
+                sx={{
+                  display: 'flex',
+                  direction: 'column',
+                  justifyContent: 'space-around',
+                  height: '100%',
+                  width: 512,
+                  p: 2
+                }}
+              >
+                <Stack
+                  alignItems='center'
+                  direction='row'
+                  justifyContent='space-between'
+                  spacing={4}
+                >
+                  <div>
+                    <Typography
+                      color='text.secondary'
+                      gutterBottom
+                      variant='overline'
+                      align='center'
+                    >
+                      Despesas
+                    </Typography>
+                  </div>
+                  <div>
+                    <Typography variant='h4'>
+                      {monthlyCost.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}
+                    </Typography>
+                  </div>
+                </Stack>
+              </Card>
+            </Stack>
+            <Grid
+              container
+              spacing={3}
+            >
+              <Grid xs={12}>
+                <Card
+                  sx={{
+                    minHeight: '70vh',
+                    height: '100%',
+                    width: '100%'
+                  }}
+                >
+                  <CardContent>
+                    <Box
                       sx={{
-                        flexDirection: 'row'
+                        display: 'flex',
+                        flexDirection: mdUp ? 'row' : 'column',
+                        justifyContent: 'space-between',
+                        maxHeight: '65px',
+                        alignItems: 'center',
+                        gap: '10px'
                       }}
                     >
-                      <div
-                        style={{ display: 'flex', justifyContent: lgUp ? 'flex-end' : 'center' }}
-                      >
-                        {!selectedTransaction && (
-                          <Button
-                            variant='contained'
-                            onClick={() => setOpenTransactionModal(true)}
-                            sx={{ mb: 2 }}
-                          >
-                            Nova Transação
-                          </Button>
-                        )}
-                      </div>
-                    </Stack>
-                  </Box>
+                      <Stack>
+                        <Stack sx={{ flexDirection: 'row' }}>
+                          <Stack sx={{ mr: 2 }}>
+                            <TransactionMonthSelector
+                              handleTransactionMonth={handleTransactionMonth}
+                            />
+                          </Stack>
 
-                  <Typography
-                    sx={{ mt: 8 }}
-                    variant='h5'
-                  >
-                    Transações
-                  </Typography>
+                          <Stack sx={{ mr: 2 }}>
+                            <TransactionYearSelector handleTransactionYear={handleTransactionYear} />
+                          </Stack>
 
-                  {mdUp
-                    ? (
-                      <TransactionTable
-                        handleTransactionSelect={handleTransactionSelect}
-                        transactions={transactions || []}
-                      />
-                      )
-                    : (
-                      <TransactionCardList
-                        transactions={transactions || []}
-                        handleTransactionSelect={handleTransactionSelect}
-                      />
-                      )}
-                </CardContent>
+                          <Stack sx={{ mr: 2 }}>
+                            <TransactionTypeSelector handleTransactionType={handleTransactionType} />
+                          </Stack>
 
-                <CardActions sx={{ justifyContent: 'center' }}>
-                  <Box
-                    sx={{
-                      display: 'flex',
-                      justifyContent: 'center'
-                    }}
-                  >
-                    {totalPages > 1 && (
-                      <Pagination
-                        onChange={(_e, value) => {
-                          setPage(value)
+                        </Stack>
+                      </Stack>
+                      <Stack
+                        sx={{
+                          flexDirection: 'row'
                         }}
-                        count={totalPages}
-                        size='small'
-                      />
-                    )}
-                  </Box>
-                </CardActions>
-              </Card>
-            </Grid>
-          </Grid>
-        </Container>
-      </Box>
+                      >
+                        <div
+                          style={{ display: 'flex', justifyContent: lgUp ? 'flex-end' : 'center' }}
+                        >
+                          {!selectedTransaction && (
+                            <Button
+                              variant='contained'
+                              onClick={() => setOpenTransactionModal(true)}
+                              sx={{ mb: 2 }}
+                            >
+                              Nova Transação
+                            </Button>
+                          )}
+                        </div>
+                      </Stack>
+                    </Box>
 
-      <TransactionsModal
-        sx={{ width: { xs: '100%', sm: '50%' }, height: 'auto' }}
-        openModal={openTransactionModal}
-        transaction={selectedTransaction}
-        refreshTransactions={refreshTransactions}
-        handleCloseModal={() => {
-          setOpenTransactionModal(false)
-          setSelectedTransaction(null)
-        }}
-      />
-    </>
-  )
+                    <Typography
+                      sx={{ mt: 8 }}
+                      variant='h5'
+                    >
+                      Transações
+                    </Typography>
+
+                    {mdUp
+                      ? (
+                        <TransactionTable
+                          handleTransactionSelect={handleTransactionSelect}
+                          transactions={transactions || []}
+                        />
+                        )
+                      : (
+                        <TransactionCardList
+                          transactions={transactions || []}
+                          handleTransactionSelect={handleTransactionSelect}
+                        />
+                        )}
+                  </CardContent>
+
+                  <CardActions sx={{ justifyContent: 'center' }}>
+                    <Box
+                      sx={{
+                        display: 'flex',
+                        justifyContent: 'center'
+                      }}
+                    >
+                      {totalPages > 1 && (
+                        <Pagination
+                          onChange={(_e, value) => {
+                            setPage(value)
+                          }}
+                          count={totalPages}
+                          size='small'
+                        />
+                      )}
+                    </Box>
+                  </CardActions>
+                </Card>
+              </Grid>
+            </Grid>
+          </Container>
+        </Box>
+
+        <TransactionsModal
+          sx={{ width: { xs: '100%', sm: '50%' }, height: 'auto' }}
+          openModal={openTransactionModal}
+          transaction={selectedTransaction}
+          refreshTransactions={refreshTransactions}
+          handleCloseModal={() => {
+            setOpenTransactionModal(false)
+            setSelectedTransaction(null)
+          }}
+        />
+      </>
+    )
+  }
 }
 
 export default Dashboard
