fdb5208b168fda956557563dd766aeedbb2e1135
Jefferson Mantovani <jefferson.mantovani@bolttech.io>
2023-09-17 19:10:31 -0300

    test: deploy

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -18,6 +18,8 @@ export default function Home () {
   const { user: currentLoggedUser } = useUser()
   const notificationCtx = useContext(NotificationContext)
 
+  console.log('currentLoggedUser ->', currentLoggedUser)
+
   async function getOrCreateUser () {
     const userToken = await getToken()
     const { data: foundUser } = await fetch('/api/user', {
